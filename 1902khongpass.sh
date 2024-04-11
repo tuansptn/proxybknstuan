eea2b978dfe7a8ca49ee16d4031e63cafff3aaa6
tuansptn <129665666+tuansptn@users.noreply.github.com>
2024-04-12 00:51:45 +0700

    Update 1902khongpass.sh

--- 1902khongpass.sh
+++ 1902khongpass.sh
@@ -31,7 +31,7 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2000
+maxconn 10000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -89,8 +89,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=62000
-LAST_PORT=63990
+FIRST_PORT=62001
+LAST_PORT=64000
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
