ab096320cc28d4504bb15d7810697b60bcc3f70b
tuansptn <129665666+tuansptn@users.noreply.github.com>
2024-04-12 00:54:33 +0700

    Update 1902khongpass.sh

--- 1902khongpass.sh
+++ 1902khongpass.sh
@@ -90,7 +90,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=62001
-LAST_PORT=64000
+LAST_PORT=63900
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
